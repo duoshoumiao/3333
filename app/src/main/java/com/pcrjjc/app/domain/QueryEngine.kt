8a358eb09e801bf5f17a5b89142db1c74842948f
莱宝 <94856184+duoshoumiao@users.noreply.github.com>
2026-04-09 15:24:38 +0800

    Update QueryEngine.kt

--- app/src/main/java/com/pcrjjc/app/domain/QueryEngine.kt
+++ app/src/main/java/com/pcrjjc/app/domain/QueryEngine.kt
@@ -5,6 +5,7 @@ import com.pcrjjc.app.data.local.entity.PcrBind
 import com.pcrjjc.app.data.remote.ApiException  
 import com.pcrjjc.app.data.remote.PcrClient  
 import com.pcrjjc.app.data.remote.TwPcrClient  
+import kotlinx.coroutines.channels.Channel  
 import kotlinx.coroutines.coroutineScope  
 import kotlinx.coroutines.launch  
   
