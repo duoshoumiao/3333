1c2b45bc58b726066a163717146f3fb97caf6699
duoshoumiao <94856184+duoshoumiao@users.noreply.github.com>
2026-04-11 13:37:17 +0800

    1

--- app/src/main/java/com/pcrjjc/app/data/remote/ApiException.kt
+++
@@ -1,6 +0,0 @@
-package com.pcrjjc.app.data.remote
-
-/**
- * API exception corresponding to ApiException in pcrclient.py
- */
-class ApiException(message: String, val code: Int) : Exception(message)
--- app/src/main/java/com/pcrjjc/app/data/remote/CaptchaRequiredException.kt
+++
@@ -1,7 +0,0 @@
-package com.pcrjjc.app.data.remote  
-  
-class CaptchaRequiredException(  
-    val gt: String,  
-    val challenge: String,  
-    val gtUserId: String  
-) : Exception("需要手动过码")
