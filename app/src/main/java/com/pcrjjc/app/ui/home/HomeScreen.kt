331b9201308d1ba134d2acd14ae836db2b01ec80
duoshoumiao <94856184+duoshoumiao@users.noreply.github.com>
2026-04-13 00:27:31 +0800

    2026年4月13日00:27:29

--- app/src/main/java/com/pcrjjc/app/ui/home/HomeScreen.kt
+++ app/src/main/java/com/pcrjjc/app/ui/home/HomeScreen.kt
@@ -36,6 +36,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme  
 import androidx.compose.material3.Scaffold  
 import androidx.compose.material3.Tab  
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.TabRow  
 import androidx.compose.material3.Text  
 import androidx.compose.material3.TopAppBar  
--- app/src/main/java/com/pcrjjc/app/ui/master/MasterScreen.kt
+++ app/src/main/java/com/pcrjjc/app/ui/master/MasterScreen.kt
@@ -52,6 +52,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope  
 import androidx.compose.runtime.setValue  
 import androidx.compose.ui.Alignment  
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.ui.Modifier  
 import androidx.compose.ui.text.input.KeyboardType  
 import androidx.compose.ui.text.input.PasswordVisualTransformation  
