4bf2a695c0b813a7398a79a9b7dd8476907318dd
duoshoumiao <94856184+duoshoumiao@users.noreply.github.com>
2026-04-19 22:52:17 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26  
         targetSdk = 34  
         versionCode = 4  
-        versionName = "3.0.14"  
+        versionName = "3.0.15"  
   
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"  
         vectorDrawables {  
