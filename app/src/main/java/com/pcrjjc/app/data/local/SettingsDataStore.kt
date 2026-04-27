c6b6ebf96073c43b2618508f309eddcf98753821
duoshoumiao <94856184+duoshoumiao@users.noreply.github.com>
2026-04-28 00:05:28 +0800

    Update SettingsDataStore.kt

--- app/src/main/java/com/pcrjjc/app/data/local/SettingsDataStore.kt
+++ app/src/main/java/com/pcrjjc/app/data/local/SettingsDataStore.kt
@@ -135,15 +135,15 @@ class SettingsDataStore(private val context: Context) {
 
   
     suspend fun getServerUrl(): String {  
-		return "http://124.220.105.217:8077"  
+		return "http://119.91.249.245:8077"  
 	}  
 	  
 	suspend fun getDailyServerUrl(): String {  
-		return "http://124.220.105.217:8040"  
+		return "http://119.91.249.245:8040"  
 	}  
 	  
 	suspend fun getRoomServerUrl(): String {  
-		return "http://124.220.105.217:8066"  
+		return "http://119.91.249.245:8066"  
 	}  
   
     // ==================== 清日常账号保存 ====================    
