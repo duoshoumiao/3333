7ccf9d49680e9f1f524b10d386f61db99424173f
duoshoumiao <94856184+duoshoumiao@users.noreply.github.com>
2026-09-16 15:32:34 +0800

    Update AutoDefScreen.kt

--- app/src/main/java/com/pcrjjc/app/ui/autodef/AutoDefScreen.kt
+++ app/src/main/java/com/pcrjjc/app/ui/autodef/AutoDefScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem  
 import androidx.compose.material3.ExperimentalMaterial3Api  
 import androidx.compose.material3.ExposedDropdownMenuBox  
-import androidx.compose.material3.ExposedDropdownMenu
 import androidx.compose.material3.FilterChip  
 import androidx.compose.material3.HorizontalDivider  
 import androidx.compose.material3.Icon  
